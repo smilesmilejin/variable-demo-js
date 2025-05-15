f683d300c5d95681a6c3746168a01bc8b593bf1b
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-15 11:26:07 -0400

    compared let, const, var

--- src/index.js
+++ src/index.js
@@ -6,9 +6,53 @@ console.log(jingsFavNum);
 console.log(jingsFavGame);
 console.log(jingsFavColor);
 
+
+// reassigning let variable
 jingsFavNum = 100;
 console.log(jingsFavNum);
+console.log("jingsFavNum: " + jingsFavNum);
+// jingsFavNum: 100
+
+
 
 // Uncomment this line to see a TypeError
 // for re-assigning a const variable:
 // jingsFavGame = 'butterfly soup';
+
+
+// reassigning var
+jingsFavColor = 'pink';
+console.log(jingsFavColor);
+console.log("jingsFavColor: " + jingsFavColor);
+// pink
+
+if (true) {
+    let message ="Hello";
+    const greeting = "Aloha";
+    var danger = "uh oh!";
+}
+
+
+
+// try let message
+// console.log(message);
+// /Users/xinshuangjin/Downloads/variable-demo-js/src/index.js:36
+
+// console.log(message);
+//             ^
+// ReferenceError: message is not defined
+//     at Object.<anonymous> (/Users/xinshuangjin/Downloads/variable-demo-js/src/index.js:36:13)
+
+
+// try greeting
+// console.log(greeting);
+//             ^
+
+// ReferenceError: greeting is not defined
+//     at Object.<anonymous> (/Users/xinshuangjin/Downloads/variable-demo-js/src/index.js:46:13)
+
+
+// try var
+// This can be accessed outside, 
+console.log(danger);
+// uh oh!
